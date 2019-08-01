7ee63c4ababd1137c6856e0d37144967b8def06c
Jake McCrary <jake@jakemccrary.com>
2019-08-01 11:24:05 -0500

    Get order the same

--- build.clj
+++ build.clj
@@ -2,6 +2,6 @@
 
 (cljs.build.api/build "src"
                       {:output-to "out/main.js"
-                       :main 'phoenix.core
                        :optimizations :simple
+                       :main 'phoenix.core
                        :externs ["phoenix-externs.js"]})
